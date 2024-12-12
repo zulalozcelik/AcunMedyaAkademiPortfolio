05e3fb5d9bfe50ed68b569f956c310b919e75bb5
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2024-12-12 22:06:19 +0300

    Add project files.

---
+++ AcunMedyaAkademiPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+        public ActionResult MesssageList()
+        {
+            return View();
+        }
+    }
+}
