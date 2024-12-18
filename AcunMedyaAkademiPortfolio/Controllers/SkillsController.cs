d4fb7d0946d14eaa507dcd1c27a85ccbb5c615a7
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2024-12-18 22:12:45 +0300

    18.12.2024

---
+++ AcunMedyaAkademiPortfolio/Controllers/SkillsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class SkillsController : Controller
+    {
+        DbPortfolioEntities db = new DbPortfolioEntities();
+        public ActionResult SkillsList()
+        {
+            var values = db.TblSkills.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(TblSkills p)
+        {
+            db.TblSkills.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("SkillsList");
+        }
+        public ActionResult DeleteSkill(int id) 
+        {
+            var value=db.TblSkills.Find(id);
+            db.TblSkills.Remove(value); 
+            db.SaveChanges();
+            return RedirectToAction("SkillsList");
+        }
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = db.TblSkills.Find(id);
+            return View(value);
+        }
+    }
+}
---
+++ AcunMedyaAkademiPortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        DbPortfolioEntities db=new DbPortfolioEntities();
+  
+        public ActionResult Index()
+        {
+            var values=db.TblTestimonial.ToList();
+            return View(values);
+        }
+    }
+}
