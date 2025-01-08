78152b3dd84e09512c0196ae80ce67a1f55a623f
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-01-08 13:22:44 +0300

    08.01.2025

--- AcunMedyaAkademiPortfolio/Controllers/ServiceController.cs
+++ AcunMedyaAkademiPortfolio/Controllers/ServiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcunMedyaAkademiPortfolio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,47 @@ namespace AcunMedyaAkademiPortfolio.Controllers
 {
     public class ServiceController : Controller
     {
-        // GET: Service
+        DbPortfolioEntities db = new DbPortfolioEntities();
         public ActionResult Index()
         {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateService()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateService(TblService p)
+        {
+            db.TblService.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.TblService.Find(id);
+            db.TblService.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var value = db.TblService.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateService(TblService p)
+        {
+            var value = db.TblService.Find(p.ServiceId);
+            value.Title = p.Title;
+            value.Description = p.Description;
+            value.IconUrl = p.IconUrl;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
