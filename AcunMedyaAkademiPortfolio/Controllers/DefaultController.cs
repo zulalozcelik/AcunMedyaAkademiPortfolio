c7f2cb7ff663096b676f4668f7c650c80a122e90
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-01-10 17:14:41 +0300

    10.01.2025

---
+++ AcunMedyaAkademiPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbPortfolioEntities db = new DbPortfolioEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            var values = db.TblFeature.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblAbout.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialSkills()
+        {
+            var values = db.TblSkills.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialStatistic()
+        {
+            var skillcount = db.TblSkills.ToList().Count();
+            ViewBag.SkillCount = skillcount;
+            return PartialView();
+        }
+    }
+}
